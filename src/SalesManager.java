cac9d20602bc9a14c978bb4f988914bcadb55474
Eugene Orlov <unimws@gmail.com>
2023-02-07 04:09:08 +0300

    Replacing int with long

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,13 +1,13 @@
 public class SalesManager {
-    protected int[] sales;
+    protected long[] sales;
 
-    public SalesManager(int[] sales) {
+    public SalesManager(long[] sales) {
         this.sales = sales;
     }
 
-    public int max() {
-        int max = -1;
-        for (int sale : sales) {
+    public long max() {
+        long max = -1;
+        for (long sale : sales) {
             if (sale > max) {
                 max = sale;
             }
